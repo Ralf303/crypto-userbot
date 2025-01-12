6da1793e7b837e4addd1d7287e9b62078161ab18
Ralf303 <nikitasuhanov34@gmail.com>
2025-01-12 16:51:31 +0300

    Изменение шрифта и корректировка обрезки изображения в функции обработки изображений

--- src/jimp.util.ts
+++ src/jimp.util.ts
@@ -1,33 +1,31 @@
 import { Jimp, loadFont } from "jimp";
-import { SANS_16_WHITE } from "jimp/fonts";
+import { SANS_64_WHITE } from "jimp/fonts";
 
 export async function processImage() {
   try {
     const image = await Jimp.read("screen.png");
 
-    // const left = 50;
-    // const right = 350;
-    // const top = 80;
-    // const bottom = 150;
+    const left = 0;
+    const right = 0;
+    const top = 0;
+    const bottom = 150;
 
-    // const croppedWidth = Math.max(1, image.bitmap.width - left - right);
-    // const croppedHeight = Math.max(1, image.bitmap.height - top - bottom);
+    const croppedWidth = Math.max(1, image.bitmap.width - left - right);
+    const croppedHeight = Math.max(1, image.bitmap.height - top - bottom);
 
-    // const croppedImage = image.crop({
-    //   x: left,
-    //   y: top,
-    //   w: croppedWidth,
-    //   h: croppedHeight,
-    // });
+    const croppedImage = image.crop({
+      x: left,
+      y: top,
+      w: croppedWidth,
+      h: croppedHeight,
+    });
 
-    const font = await loadFont(SANS_16_WHITE);
+    const font = await loadFont(SANS_64_WHITE);
 
     const watermarkText = "© RT AI TRADE";
 
-    // const x = croppedImage.bitmap.width - 500;
-    // const y = croppedImage.bitmap.height - 100;
-    const x = image.bitmap.width;
-    const y = image.bitmap.height - 100;
+    const x = croppedImage.bitmap.width - 500;
+    const y = croppedImage.bitmap.height - 100;
 
     image.print({ font, x, y, text: watermarkText });
 
